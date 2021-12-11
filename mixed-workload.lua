7f25532084c5ec85ba530c2f3ee859caa74bc711
Xiangfeng Zhu <xzhu0027@gmail.com>
2021-12-10 16:34:19 -0800

    tweaks

--- mixed-workload.lua
+++ mixed-workload.lua
@@ -12,7 +12,7 @@ products = {
     'L9ECAV7KIM',
     'LS4PSXUNUM',
     'OLJCESPC7Z'}
-
+-- curl -d "product_id=0PUK6V6EV0&quantity=2" -X POST http://10.111.134.111/cart -v
 local function index()
   local method = "GET"
   local path = "http://localhost/"
@@ -44,39 +44,8 @@ local function view_cart()
   local headers = {}
   return wrk.format(method, path, headers, nil)
 
-local function checkout()
-  local currencies = {'EUR', 'USD', 'JPY', 'CAD'}
-  local method = "POST"
-  local body = "currency_code=" .. currencies[math.random(#currencies)]
-  local path = "http://localhost/setCurrency/"
-
-  local headers = {}
-  return wrk.format(method, path, headers, body)
-
-local function user_login()
-  local user_name, password = get_user()
-  local method = "GET"
-  local path = "http://localhost:5000/user?username=" .. user_name .. "&password=" .. password
-  local headers = {}
-  -- headers["Content-Type"] = "application/x-www-form-urlencoded"
-  return wrk.format(method, path, headers, nil)
-end
 
 request = function()
-  cur_time = math.floor(socket.gettime())
-  local search_ratio      = 0.6
-  local recommend_ratio   = 0.39
-  local user_ratio        = 0.005
-  local reserve_ratio     = 0.005
-
-  local coin = math.random()
-  if coin < search_ratio then
-    return search_hotel()
-  elseif coin < search_ratio + recommend_ratio then
-    return recommend()
-  elseif coin < search_ratio + recommend_ratio + user_ratio then
-    return user_login()
-  else 
-    return reserve()
+    return index()
   end
 end
